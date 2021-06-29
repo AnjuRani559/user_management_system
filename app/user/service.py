0efbb12a47dcd6e087973455f0d633ba531ab299
anjurani559 <anju.rani@wanclouds.net>
2021-06-29 10:59:24 +0530

    changed code

--- app/user/service.py
+++ app/user/service.py
@@ -5,25 +5,20 @@ from .interface import UserInterface
 from sqlalchemy_filters import apply_filters
 
 
-class Userservice():
-    class UserService:
-        @staticmethod
-        def get_all(filters) -> List[User]:
-            f_query = db.session.query(User)
-            # add filters from list of filter
-            if len(filters) > 0:
-                f_query = apply_filters(f_query, filters)
-                return f_query.all()
-
+class Userservice:
     @staticmethod
-    def get_all(self)->[User]:
-        return User.query.all()
-
+    def get_all(filters) -> List[User]:
+        f_query = db.session.query(User)
+        # add filters from list of filter
+        if len(filters) > 0:
+            f_query = apply_filters(f_query, filters)
+        return f_query.all()
 
     @staticmethod
     def create(new_attrs: UserInterface) -> User:
         new_user = User(
-        first_name=new_attrs["first_name"], last_name=new_attrs["last_name"], full_name=new_attrs["full_name"], created_at=new_attrs["created_at"], salary=  new_attrs["salary"])
+            first_name=new_attrs["first_name"], last_name=new_attrs["last_name"], full_name=new_attrs["full_name"],
+            created_at=new_attrs["created_at"], salary=new_attrs["salary"])
         db.session.add(new_user)
         db.session.commit()
 
@@ -47,7 +42,3 @@ class Userservice():
         db.session.delete(user)
         db.session.commit()
         return [user_id]
-
-
-
-
